0b9ace4768f409786768a3e267e3c52bc7e3778e
Prabh <133885553+90541118325@users.noreply.github.com>
2024-12-07 21:00:00 +0530

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <Header />
     <NuxtPage />
-    <Footer />
   </div>
 </template>
 
 <script setup>
-import Header from '~/components/Header.vue';
-import Footer from '~/components/Footer.vue';
+// import Header from '~/components/Header.vue';
+// import Footer from '~/components/Footer.vue';
 </script>
 
 <style>
