5bd202131542a578f4e618f63e9e23ac589c6de6
Prabh <133885553+90541118325@users.noreply.github.com>
2024-12-07 20:45:06 +0530

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -5,10 +5,10 @@
       <div class="container">
         <router-link to="/" class="navbar-logo">Prabhdeep Singh</router-link>
         <ul class="navbar-links">
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/about">About</router-link></li>
-          <li><router-link to="/blog">Blog</router-link></li>
-          <li><router-link to="/contact">Contact</router-link></li>
+          <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/">Home</router-link></li>
+          <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/about/">About</router-link></li>
+          <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io//blog">Blog</router-link></li>
+          <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io//contact">Contact</router-link></li>
         </ul>
       </div>
     </nav>
