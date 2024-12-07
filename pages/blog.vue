c420018a6a8d1d1290594de877e1ad2dd2083286
Prabh <133885553+90541118325@users.noreply.github.com>
2024-12-07 20:45:51 +0530

    Update blog.vue

--- pages/blog.vue
+++ pages/blog.vue
@@ -5,10 +5,10 @@
         <div class="container">
           <router-link to="/" class="navbar-logo">Prabhdeep Singh</router-link>
           <ul class="navbar-links">
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/about">About</router-link></li>
-            <li><router-link to="/blog" class="active-link">Blog</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/">Home</router-link></li>
+            <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/about">About</router-link></li>
+            <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/blog" class="active-link">Blog</router-link></li>
+            <li><router-link to="https://90541118325.github.io/prabhsidhu.github.io/contact">Contact</router-link></li>
           </ul>
         </div>
       </nav>
@@ -18,7 +18,7 @@
         <div class="hero-content">
           <h1>Welcome to My Blog</h1>
           <p>Stay updated with my latest thoughts and articles.</p>
-          <NuxtLink to="/about" class="cta-button">Learn More About Me</NuxtLink>
+          <NuxtLink to="https://90541118325.github.io/prabhsidhu.github.io/about" class="cta-button">Learn More About Me</NuxtLink>
         </div>
       </section>
   
