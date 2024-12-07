8ecb8dc00b538f3ad9f526342f9140a53500e901
Prabh <133885553+90541118325@users.noreply.github.com>
2024-12-07 20:02:15 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,9 @@
         </div>
         <ul class="navbar-links" :class="{ 'active': menuActive }">
           <li><router-link to="index.vue/">Home</router-link></li>
-          <li><router-link to="about">About</router-link></li>
-          <li><router-link to="blog">Blog</router-link></li>
-          <li><router-link to="contact">Contact</router-link></li>
+          <li><router-link to="about.vue">About</router-link></li>
+          <li><router-link to="blog.vue">Blog</router-link></li>
+          <li><router-link to="contact.vue">Contact</router-link></li>
         </ul>
       </div>
     </nav>
